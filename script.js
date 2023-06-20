74c4adef0d5c12752e332199249395c5596e6968
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-06-20 18:04:23 +1000

    Create grid and provide mouseover effect

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 // create grid layout
 const gridSquares = document.querySelector('#grid-squares');
 
-for (let i = 0; i < 16; i++) {
-    const column = document.createElement('div');
-    column.setAttribute('class', 'column');
-    gridSquares.appendChild(column);
+let numberOfSquares = 16;
+createSquares(numberOfSquares);
+
+// function getNumberOfSquares() {
+//     numberOfSquares = 
+// }
+
+function createSquares(numberOfSquares) {
+    for (let i = 0; i < numberOfSquares; i++) {
+        const column = document.createElement('div');
+        column.setAttribute('class', 'column');
+        gridSquares.appendChild(column);
 
 
-    for (let j = 0; j < 16; j++) {
-        const row = document.createElement('div');
-        column.appendChild(row);
+        for (let j = 0; j < numberOfSquares; j++) {
+            const row = document.createElement('div');
+            row.setAttribute('class', 'grids');
+            column.appendChild(row);
+        }
     }
 }
+
+// mouseover change color
+const divGrid = document.querySelectorAll('.grids');
+divGrid.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+        div.classList.toggle('color-change');
+    })
+})
+
