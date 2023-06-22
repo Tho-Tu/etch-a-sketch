9c9aa60970c68a43da8919f71ca523c9f1a39ac9
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-06-23 08:03:24 +1000

    Allow user to change colors

--- script.js
+++ script.js
@@ -9,7 +9,24 @@ const displaySquares = document.querySelector('.number-of-squares');
 changeSquares.addEventListener('input', () => {
     displaySquares.textContent = `${changeSquares.value}x${changeSquares.value}`;
     createNewSquares();
-    colorChange();
+    draw(newColor);
+});
+
+// changing color of draw() 
+let newColor = null;
+const chooseColor = document.querySelector('#choose-color');
+chooseColor.addEventListener('change', () => {
+    newColor = chooseColor.value;
+    draw(newColor);
+});
+
+// reset button
+const resetButton = document.querySelector('#reset-button');
+resetButton.addEventListener('click', () => {
+    createNewSquares();
+    newColor = null;
+    draw(newColor);
+    chooseColor.value = '#ff7f50';
 });
 
 function createNewSquares() {
@@ -24,7 +41,6 @@ function createSquares(numberOfSquares) {
         column.setAttribute('class', 'column');
         gridSquares.appendChild(column);
 
-
         for (let j = 0; j < numberOfSquares; j++) {
             const row = document.createElement('div');
             row.setAttribute('class', 'grids');
@@ -33,23 +49,42 @@ function createSquares(numberOfSquares) {
     }
 }
 
-// reset button
-const resetButton = document.querySelector('#reset-button');
-resetButton.addEventListener('click', () => {
-    createNewSquares();
-    colorChange();
-});
-
-// mouseover change color
-function colorChange() {
+function draw(newColor) {
     const divGrid = document.querySelectorAll('.grids');
+    let isDrawing = false;
     divGrid.forEach((div) => {
+        div.addEventListener('mousedown', () => {
+            isDrawing = true;
+            if (newColor !== null) {
+                div.setAttribute('style', `background-color: ${newColor}`);
+            }
+            else if (isDrawing === true) {
+                div.classList.add('color-change');
+            }
+        });
         div.addEventListener('mouseover', () => {
-            div.classList.toggle('color-change');
+            if (newColor !== null && isDrawing === true) {
+                div.setAttribute('style', `background-color: ${newColor}`);
+            }
+            else if (isDrawing === true) {
+                div.classList.add('color-change');
+            }
+        });
+        div.addEventListener('mouseup', () => {
+            isDrawing = false;
         });
     });
 }
 
-colorChange();
+// call draw function for when webpage loads 
+draw(newColor);
+
+function rainbowColor() {
+    let redColor = Math.floor(Math.random() * 255);
+    let greenColor = Math.floor(Math.random() * 255);
+    let blueColor = Math.floor(Math.random() * 255);
+
+    return randomRGB = rgb(redColor, greenColor, blueColor);
+}
 
 
