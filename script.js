16dfd6b0b1172163bc0ac789045b15940cf0e2d2
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-06-23 18:49:08 +1000

    Create rainbow & random color button
    The Javascript code was also cleaned by grouping the event listeners
    and grouping the functions separately. Also, boolean variables were
    moved to the top.

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+// --- INITIAL STARTUP OF WEBPAGE --- //
+
+
+let newColor = null; // allows user to set their own color
+let isRainbow = false; // allows user to change to rainbow
+
 // create initial grid layout
 const gridSquares = document.querySelector('#grid-squares');
 let numberOfSquares = 16;
 createSquares(numberOfSquares);
+// call draw function for when webpage loads 
+draw(newColor);
 
-// dynamically change displaySquares text before apply function
+
+// --- EVENT LISTENERS --- // 
+
+
+// dynamically change displaySquares text 
 const changeSquares = document.querySelector('.slider');
 const displaySquares = document.querySelector('.number-of-squares');
 changeSquares.addEventListener('input', () => {
@@ -12,22 +24,43 @@ changeSquares.addEventListener('input', () => {
     draw(newColor);
 });
 
-// changing color of draw() 
-let newColor = null;
-const chooseColor = document.querySelector('#choose-color');
-chooseColor.addEventListener('change', () => {
-    newColor = chooseColor.value;
-    draw(newColor);
-});
-
 // reset button
 const resetButton = document.querySelector('#reset-button');
 resetButton.addEventListener('click', () => {
     createNewSquares();
     newColor = null;
+    isRainbow = false;
     draw(newColor);
     chooseColor.value = '#ff7f50';
 });
+
+// changing color of draw() 
+const chooseColor = document.querySelector('#choose-color');
+chooseColor.addEventListener('change', () => {
+    newColor = chooseColor.value;
+    isRainbow = false;
+    draw(newColor);
+});
+
+// randomize color 
+const randomButton = document.querySelector('#random-color');
+randomButton.addEventListener('click', () => {
+    tempColor = randomColor();
+    chooseColor.value = tempColor;
+    newColor = null;
+    isRainbow = false; 
+    draw(tempColor);
+});
+
+// draw rainbow
+const rainbowButton = document.querySelector('#rainbow-color');
+rainbowButton.addEventListener('click', () => {
+    isRainbow = true;
+});
+
+
+// --- FUNCTIONS --- // 
+
 
 function createNewSquares() {
     gridSquares.textContent = '';
@@ -63,7 +96,10 @@ function draw(newColor) {
             }
         });
         div.addEventListener('mouseover', () => {
-            if (newColor !== null && isDrawing === true) {
+            if (isRainbow === true && isDrawing === true) {
+                div.setAttribute('style', `background-color: ${randomColor()}`);
+            }
+            else if (newColor !== null && isDrawing === true) {
                 div.setAttribute('style', `background-color: ${newColor}`);
             }
             else if (isDrawing === true) {
@@ -76,15 +112,21 @@ function draw(newColor) {
     });
 }
 
-// call draw function for when webpage loads 
-draw(newColor);
+function randomColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
 
-function rainbowColor() {
-    let redColor = Math.floor(Math.random() * 255);
-    let greenColor = Math.floor(Math.random() * 255);
-    let blueColor = Math.floor(Math.random() * 255);
-
-    return randomRGB = rgb(redColor, greenColor, blueColor);
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+  
+    if (r.length == 1)
+      r = "0" + r;
+    if (g.length == 1)
+      g = "0" + g;
+    if (b.length == 1)
+      b = "0" + b;
+  
+    return "#" + r + g + b;
 }
-
-
